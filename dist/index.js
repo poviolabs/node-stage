1c8282ea2fc8b67b0c086df0fa09d6fe9b802253
Marko Zabreznik <marko@zabreznik.net>
2022-06-22 15:50:36 +0200

    fix: exports for types

--- dist/index.js
+++ dist/index.js
@@ -14,8 +14,6 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./helpers/yargs.helper"), exports);
-__exportStar(require("./helpers/git.helper"), exports);
 __exportStar(require("./helpers/config.helper"), exports);
 __exportStar(require("./helpers/version.helper"), exports);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export * from "./helpers/yargs.helper";
-export * from "./helpers/git.helper";
 export * from "./helpers/config.helper";
 export * from "./helpers/version.helper";
